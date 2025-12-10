39ab941ada4ddfbeaf04571c063e7f49d1f4a48c
exc830 <edward.cabrera@sherwin.com>
2025-12-10 09:42:09 -0500

    up
    And adding async storage module

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -1,17 +1,25 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnboardingScreen from "../screens/OnboardingScreen";
+import HomeScreen from "../screens/Home";
+import ProfileScreen from "../screens/Profile";
+import SplashScreen from "../screens/SplashScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
+  if (state.isLoading) {
+    return <SplashScreen />;
+  }
+
   return (
     <Stack.Navigator initialRouteName="Onboarding">
-      <Stack.Screen
-        options={{ title: "Onboarding" }}
-        name="Onboarding"
-        component={OnboardingScreen}
-      ></Stack.Screen>
+      {state.isOnboardingCompleted ? (
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      ) : (
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      )}
     </Stack.Navigator>
   );
 };
